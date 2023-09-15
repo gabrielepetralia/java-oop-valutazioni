83329adf1a23630c385086c90fa8f8f95f58bbf1
gabrielepetralia <gabri.petralia03@gmail.com>
2023-09-15 18:01:10 +0200

    add bonus

--- src/org/lessons/java/valutazioni/Main.java
+++ src/org/lessons/java/valutazioni/Main.java
@@ -1,5 +1,6 @@
 package org.lessons.java.valutazioni;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -16,7 +17,7 @@ public class Main {
 			
 			int id = i + 1;
 			int percentualeAssenze = rnd.nextInt(0,101);
-			float mediaVoti = rnd.nextFloat(0,6);
+			float mediaVoti = rnd.nextFloat(0,5.01f);
 			
 			studenti[i] = new Studente(id, percentualeAssenze, mediaVoti);
 			
@@ -35,6 +36,41 @@ public class Main {
 			System.out.println("\n==============================\n");
 		}
 		
+		Studente[] studentiPromossi = new Studente[countPromoted];
+		Studente[] studentiBocciati = new Studente[lng - countPromoted];
+		
 		System.out.println("Numero studenti promossi: " + countPromoted + " su " + lng);
+		
+		//Bonus 
+		int indexPromossi = 0;
+		int indexBocciati = 0;
+		
+		for(int i=0; i<lng; i++) {
+			if(studenti[i].isPromoted()) {
+				studentiPromossi[indexPromossi] = studenti[i];
+				indexPromossi++;
+			} else {
+				studentiBocciati[indexBocciati] = studenti[i];
+				indexBocciati++;
+			}
+		}
+		
+		int indexMigliorStudente = 0;
+		int indexPeggiorStudente = 0;
+		
+		for(int i=0; i<countPromoted; i++) {
+			if(studentiPromossi[i].mediaVoti > studentiPromossi[indexMigliorStudente].mediaVoti) {
+				indexMigliorStudente = i;
+			}
+		}
+		
+		for(int i=0; i<(lng - countPromoted); i++) {
+			if(studentiBocciati[i].mediaVoti < studentiBocciati[indexPeggiorStudente].mediaVoti) {
+				indexPeggiorStudente = i;
+			}
+		}
+		
+		System.out.println("\nL'ID del miglior studente è: " + studentiPromossi[indexMigliorStudente].id);
+		System.out.println("L'ID del peggior studente è: " + studentiBocciati[indexPeggiorStudente].id);
 	}
 }
